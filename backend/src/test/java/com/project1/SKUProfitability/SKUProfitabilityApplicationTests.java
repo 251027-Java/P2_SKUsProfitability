8656484f43030ff2618b83d54f8bfa924f54b06d
Raman <shrestha.raman1126@gmail.com>
2025-12-24 00:12:34 -0600

    started test file

---
+++ backend/src/test/java/com/project1/SKUProfitability/SKUProfitabilityApplicationTests.java
@@ -0,0 +1,14 @@
+package com.project1.SKUProfitability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SKUProfitabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+

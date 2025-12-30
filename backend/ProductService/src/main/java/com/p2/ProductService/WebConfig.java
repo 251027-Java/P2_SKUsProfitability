fe393c008360b75380484666647ad0d08f8b34be
hg2025 <90288992+hg2025@users.noreply.github.com>
2025-12-29 21:47:36 -0600

    Update WebConfig.java

--- backend/ProductService/src/main/java/com/p2/ProductService/WebConfig.java
+++ backend/ProductService/src/main/java/com/p2/ProductService/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "/api/skus",
                         "/api/lists",
                         "/api/skus/**",
-                        "/api/skus/**"
+                        "/api/list/**"
                 );
     }
 

2e4914a5fceba5c720b05bd18733d2a9793ad02c
FahadNawazCs <fahadnawaz.cs99@gmail.com>
2025-12-31 17:48:41 -0600

    Created api request file

---
+++ backend/product-service/src/main/java/com/p2/product_service/model/request/DataBrightRequest.java
@@ -0,0 +1,12 @@
+package com.p2.product_service.model.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class DataBrightRequest {
+     List<ProductRequest> input;
+}

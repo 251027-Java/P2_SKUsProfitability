21d9ed63d9e0140042059b0f469bcda857eaf979
Raman <shrestha.raman1126@gmail.com>
2026-01-07 01:33:43 -0600

    organized files

--- backend/SKUProfitability/src/main/java/com/project2/SKUProfitability/DTO/ListCreateDTO.java
+++
@@ -1,7 +0,0 @@
-package com.project2.SKUProfitability.DTO;
-
-public record ListCreateDTO(
-        String name,
-        String description
-) {}
-
--- backend/SKUProfitability/src/main/java/com/project2/SKUProfitability/DTO/RegisterCustomerDTO.java
+++
@@ -1,9 +0,0 @@
-package com.project2.SKUProfitability.DTO;
-
-
-public record RegisterCustomerDTO(
-        String email,
-        String password,
-        String firstName,
-        String lastName
-) {}
